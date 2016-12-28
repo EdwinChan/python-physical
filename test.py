8abd91027b141a9608317cd1b9c354c7e1e96977
Edwin Chan <photon.engine@gmail.com>
2016-12-28 16:46:47 +0200

    Simplify variable names in unit tests slightly

--- test.py
+++ test.py
@@ -2,8 +2,13 @@ import fractions
 import math
 import unittest
 
-from physical.core import Quantity
-from physical.define import defined_systems, si_system
+from .core import Quantity
+from .define import defined_systems
+
+si = defined_systems['si']
+esu = defined_systems['esu']
+emu = defined_systems['emu']
+gauss = defined_systems['gauss']
 
 class PhysicalQuantitiesTest(unittest.TestCase):
   def assert_quantity_equal(self, first, second):
@@ -13,8 +18,8 @@ class PhysicalQuantitiesTest(unittest.TestCase):
     self.assertEqual(first.system, second.system)
 
   def test_sign(self):
-    a = Quantity(1, 0.2, {'Kilogram': 1}, si_system)
-    b = Quantity(-1, 0.2, {'Kilogram': 1}, si_system)
+    a = Quantity(1, 0.2, {'Kilogram': 1}, si)
+    b = Quantity(-1, 0.2, {'Kilogram': 1}, si)
     self.assert_quantity_equal(+a, a)
     self.assert_quantity_equal(+b, b)
     self.assert_quantity_equal(-a, b)
@@ -23,72 +28,72 @@ class PhysicalQuantitiesTest(unittest.TestCase):
     self.assert_quantity_equal(abs(b), a)
 
   def test_add(self):
-    a = Quantity(1, 0.2, {'Newton': 1}, si_system)
-    b = Quantity(3, 0.4, {'Kilogram': 1, 'Meter': 1, 'Second': -2}, si_system)
-    c = Quantity(4, 1 / math.sqrt(5), {'Newton': 1}, si_system)
-    d = Quantity(1, 0.2, {'Kilogram': 1}, si_system)
+    a = Quantity(1, 0.2, {'Newton': 1}, si)
+    b = Quantity(3, 0.4, {'Kilogram': 1, 'Meter': 1, 'Second': -2}, si)
+    c = Quantity(4, 1 / math.sqrt(5), {'Newton': 1}, si)
+    d = Quantity(1, 0.2, {'Kilogram': 1}, si)
     self.assert_quantity_equal(a + b, c.expand())
     with self.assertRaises(TypeError): a + d
     with self.assertRaises(TypeError): a + 1
 
   def test_subtract(self):
-    a = Quantity(1, 0.2, {'Newton': 1}, si_system)
-    b = Quantity(3, 0.4, {'Kilogram': 1, 'Meter': 1, 'Second': -2}, si_system)
-    c = Quantity(-2, 1 / math.sqrt(5), {'Newton': 1}, si_system)
-    d = Quantity(1, 0.2, {'Kilogram': 1}, si_system)
+    a = Quantity(1, 0.2, {'Newton': 1}, si)
+    b = Quantity(3, 0.4, {'Kilogram': 1, 'Meter': 1, 'Second': -2}, si)
+    c = Quantity(-2, 1 / math.sqrt(5), {'Newton': 1}, si)
+    d = Quantity(1, 0.2, {'Kilogram': 1}, si)
     self.assert_quantity_equal(a - b, c.expand())
     with self.assertRaises(TypeError): a - d
     with self.assertRaises(TypeError): a - 1
 
   def test_multiply(self):
-    a = Quantity(1, 0.2, {'Kilogram': 1}, si_system)
-    b = Quantity(3, 0.4, {'Meter': -2}, si_system)
-    c = Quantity(3, math.sqrt(13) / 5, {'Kilogram': 1, 'Meter': -2}, si_system)
+    a = Quantity(1, 0.2, {'Kilogram': 1}, si)
+    b = Quantity(3, 0.4, {'Meter': -2}, si)
+    c = Quantity(3, math.sqrt(13) / 5, {'Kilogram': 1, 'Meter': -2}, si)
     self.assert_quantity_equal(a * b, c)
-    a = Quantity(1, 0.2, {'Kilogram': 1}, si_system) * 5
-    b = Quantity(5, 1, {'Kilogram': 1}, si_system)
+    a = Quantity(1, 0.2, {'Kilogram': 1}, si) * 5
+    b = Quantity(5, 1, {'Kilogram': 1}, si)
     self.assert_quantity_equal(a, b)
-    a = Quantity(1, 0.2, {'Kilogram': 1}, si_system) * -5
-    b = Quantity(-5, 1, {'Kilogram': 1}, si_system)
+    a = Quantity(1, 0.2, {'Kilogram': 1}, si) * -5
+    b = Quantity(-5, 1, {'Kilogram': 1}, si)
     self.assert_quantity_equal(a, b)
-    a = 5 * Quantity(3, 0.4, {'Kilogram': 1}, si_system)
-    b = Quantity(15, 2, {'Kilogram': 1}, si_system)
+    a = 5 * Quantity(3, 0.4, {'Kilogram': 1}, si)
+    b = Quantity(15, 2, {'Kilogram': 1}, si)
     self.assert_quantity_equal(a, b)
-    a = -5 * Quantity(3, 0.4, {'Kilogram': 1}, si_system)
-    b = Quantity(-15, 2, {'Kilogram': 1}, si_system)
+    a = -5 * Quantity(3, 0.4, {'Kilogram': 1}, si)
+    b = Quantity(-15, 2, {'Kilogram': 1}, si)
     self.assert_quantity_equal(a, b)
 
   def test_divide(self):
-    a = Quantity(2, 0.1, {'Kilogram': 1}, si_system)
-    b = Quantity(4, 0.3, {'Meter': -2}, si_system)
+    a = Quantity(2, 0.1, {'Kilogram': 1}, si)
+    b = Quantity(4, 0.3, {'Meter': -2}, si)
     c = Quantity(0.5, math.sqrt(13) / 80,
-      {'Kilogram': 1, 'Meter': 2}, si_system)
+      {'Kilogram': 1, 'Meter': 2}, si)
     self.assert_quantity_equal(a / b, c)
-    a = Quantity(1, 0.2, {'Kilogram': 1}, si_system) / 5
-    b = Quantity(0.2, 0.04, {'Kilogram': 1}, si_system)
+    a = Quantity(1, 0.2, {'Kilogram': 1}, si) / 5
+    b = Quantity(0.2, 0.04, {'Kilogram': 1}, si)
     self.assert_quantity_equal(a, b)
-    a = Quantity(1, 0.2, {'Kilogram': 1}, si_system) / -5
-    b = Quantity(-0.2, 0.04, {'Kilogram': 1}, si_system)
+    a = Quantity(1, 0.2, {'Kilogram': 1}, si) / -5
+    b = Quantity(-0.2, 0.04, {'Kilogram': 1}, si)
     self.assert_quantity_equal(a, b)
-    a = 5 / Quantity(3, 0.4, {'Kilogram': 1}, si_system)
-    b = Quantity(5/3, 2/9, {'Kilogram': -1}, si_system)
+    a = 5 / Quantity(3, 0.4, {'Kilogram': 1}, si)
+    b = Quantity(5/3, 2/9, {'Kilogram': -1}, si)
     self.assert_quantity_equal(a, b)
-    a = -5 / Quantity(3, 0.4, {'Kilogram': 1}, si_system)
-    b = Quantity(-5/3, 2/9, {'Kilogram': -1}, si_system)
+    a = -5 / Quantity(3, 0.4, {'Kilogram': 1}, si)
+    b = Quantity(-5/3, 2/9, {'Kilogram': -1}, si)
     self.assert_quantity_equal(a, b)
 
   def test_power(self):
-    a = Quantity(3, 0.4, {'Kilogram': 1, 'Meter': 1}, si_system) ** 5
-    b = Quantity(243, 162, {'Kilogram': 5, 'Meter': 5}, si_system)
+    a = Quantity(3, 0.4, {'Kilogram': 1, 'Meter': 1}, si) ** 5
+    b = Quantity(243, 162, {'Kilogram': 5, 'Meter': 5}, si)
     self.assert_quantity_equal(a, b)
 
   def test_almost_equals(self):
-    a = Quantity(1, 0.5, {'Kilogram': 1}, si_system)
-    b = Quantity(2, 0.7, {'Kilogram': 1}, si_system)
-    c = Quantity(3, 0.9, {'Kilogram': 1}, si_system)
-    d = Quantity(1, 0.5, {'Meter': 1}, si_system)
-    e = Quantity(1, 0.5, {}, si_system)
-    f = Quantity(2, 0.7, {}, si_system)
+    a = Quantity(1, 0.5, {'Kilogram': 1}, si)
+    b = Quantity(2, 0.7, {'Kilogram': 1}, si)
+    c = Quantity(3, 0.9, {'Kilogram': 1}, si)
+    d = Quantity(1, 0.5, {'Meter': 1}, si)
+    e = Quantity(1, 0.5, {}, si)
+    f = Quantity(2, 0.7, {}, si)
     self.assertTrue(a.almost_equals(b))
     self.assertFalse(a.almost_equals(c))
     self.assertRaises(TypeError, a.almost_equals, d)
@@ -101,28 +106,28 @@ class PhysicalQuantitiesTest(unittest.TestCase):
     self.assertTrue(e.almost_equals(f))
 
   def test_float(self):
-    a = Quantity(1, 0, {'Second': 1, 'Hertz': 1}, si_system)
-    b = Quantity(365.25 * 86400, 0, {'Second': 1, 'JulianYear': -1}, si_system)
+    a = Quantity(1, 0, {'Second': 1, 'Hertz': 1}, si)
+    b = Quantity(365.25 * 86400, 0, {'Second': 1, 'JulianYear': -1}, si)
     self.assertEqual(math.cos(a), math.cos(1))
     self.assertEqual(math.cos(b), math.cos(1))
 
   def test_expand(self):
     # Lorentz force
     a = Quantity(1, 0,
-      {'Coulomb': 1, 'Meter': 1, 'Second': -1, 'Tesla': 1}, si_system)
-    b = Quantity(1, 0, {'Newton': 1}, si_system)
+      {'Coulomb': 1, 'Meter': 1, 'Second': -1, 'Tesla': 1}, si)
+    b = Quantity(1, 0, {'Newton': 1}, si)
     self.assert_quantity_equal(a.expand(), b.expand())
     # Faraday's law
-    a = Quantity(1, 0, {'Weber': 1, 'Second': -1}, si_system)
-    b = Quantity(1, 0, {'Volt': 1}, si_system)
+    a = Quantity(1, 0, {'Weber': 1, 'Second': -1}, si)
+    b = Quantity(1, 0, {'Volt': 1}, si)
     self.assert_quantity_equal(a.expand(), b.expand())
     # torque of a motor
-    a = Quantity(1, 0, {'Ampere': 1, 'Tesla': 1, 'Meter': 2}, si_system)
-    b = Quantity(1, 0, {'Newton': 1, 'Meter': 1}, si_system)
+    a = Quantity(1, 0, {'Ampere': 1, 'Tesla': 1, 'Meter': 2}, si)
+    b = Quantity(1, 0, {'Newton': 1, 'Meter': 1}, si)
     self.assert_quantity_equal(a.expand(), b.expand())
     # resonance frequency of an RLC circuit
-    a = Quantity(1, 0, {'Henry': -1/2, 'Farad': -1/2}, si_system)
-    b = Quantity(1, 0, {'Hertz': 1}, si_system)
+    a = Quantity(1, 0, {'Henry': -1/2, 'Farad': -1/2}, si)
+    b = Quantity(1, 0, {'Hertz': 1}, si)
     self.assert_quantity_equal(a.expand(), b.expand())
 
   def test_simple_constants(self):
@@ -136,7 +141,7 @@ class PhysicalQuantitiesTest(unittest.TestCase):
       self.assertEqual(a.units, {})
 
   def test_electromagnetic_constants(self):
-    from physical import si, esu, emu, gauss
+    from . import si, esu, emu, gauss
     a = (si.e**2 / si.a0 / (4*math.pi*si.epsilon0) / (1e-7*si.J)).expand()
     b = (esu.e**2 / esu.a0 / esu.erg).expand()
     c = (emu.e**2 / emu.a0 * emu.c**2 / emu.erg).expand()
